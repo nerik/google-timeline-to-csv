2270884dffb3e56734e1a39de11b378a7ea7845f
nerik <erik.escoffier@gmail.com>
2015-11-28 20:46:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+/* eslint-env node, es6 */
+
+var fs = require("fs");
+var path = require("path");
+var xmldoc = require('xmldoc');
+var json2csv = require('json2csv');
+
+var dir = process.argv[2];
+var files = fs.readdirSync(dir);
+var fileRegex = /^history-([\d-]+)\.kml$/i;
+
+var points = [];
+
+files.filter(file => {
+  return fileRegex.test(file);
+}).forEach(file => {
+  var day = fileRegex.exec(file)[1];
+  var xmlFile = fs.readFileSync(path.join(dir, file), "utf8");
+  var xmlDocument = new xmldoc.XmlDocument(xmlFile);
+  var track = xmlDocument.descendantWithPath("Document.Placemark.gx:Track").children;
+
+  for (var i = 0; i < track.length; i++) {
+    var when = track[i];
+    if (when.name === "when") {
+      var coord = track[i+1];
+      var lonlat = coord.val.split(" ");
+      points.push({
+        when: when.val,
+        latitude: lonlat[1],
+        longitude: lonlat[0],
+        day: day
+      })
+    }
+  }
+});
+
+json2csv({ data: points }, function(err, csv) {
+  if (err) console.log(err);
+  console.log(csv);
+});
