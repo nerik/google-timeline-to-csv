b33e15f29d58a5deca1003b594c4e8679a4cd013
nerik <erik.escoffier@gmail.com>
2015-12-01 12:55:53 +0100

    fixed timezone issue

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ var fs = require("fs");
 var path = require("path");
 var xmldoc = require('xmldoc');
 var json2csv = require('json2csv');
+var moment = require('moment');
 
 var dir = process.argv[2];
 var files = fs.readdirSync(dir);
 var fileRegex = /^history-([\d-]+)\.kml$/i;
 
 var points = [];
-
 files.filter(file => {
   return fileRegex.test(file);
 }).forEach(file => {
-  var day = fileRegex.exec(file)[1];
   var xmlFile = fs.readFileSync(path.join(dir, file), "utf8");
   var xmlDocument = new xmldoc.XmlDocument(xmlFile);
+
   var track = xmlDocument.descendantWithPath("Document.Placemark.gx:Track").children;
 
   for (var i = 0; i < track.length; i++) {
@@ -27,10 +27,9 @@ files.filter(file => {
       var coord = track[i+1];
       var lonlat = coord.val.split(" ");
       points.push({
-        when: when.val,
+        when: moment(when.val).format(),
         latitude: lonlat[1],
         longitude: lonlat[0],
-        day: day
       })
     }
   }
